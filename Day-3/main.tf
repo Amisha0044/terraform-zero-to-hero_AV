06005abb1812985cb673f54cab4c169106c2625a
Amisha0044 <amisha.sharma0044@gmail.com>
2025-05-05 15:02:02 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
+module "ec2_instance" {               # referencing the module
+  source = "./modules/ec2_instance"   # path of module where it is located and has to be searched at
+  # either create .tfvars file or hardcode here
   ami_value = "ami-053b0d53c279acc90" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-019ea91ed9b5252e7" # replace this
 }
